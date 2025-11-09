019249da064ba611994377497db667152a56ab29
Alexisxde <olivarezalexis749@gmail.com>
2025-11-09 20:34:22 -0300

    fix(config): correct DATABASE_URL default path and ensure authToken is included in db client

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "drizzle-kit"
-import { DATABASE_URL, NODE_ENV } from "./src/config"
+import { DATABASE_AUTH_TOKEN, DATABASE_URL, NODE_ENV } from "./src/config"
 
 export default defineConfig({
 	dialect: NODE_ENV === "production" ? "turso" : "sqlite",
 	schema: "./src/db/schema.ts",
 	out: "./migrations",
 	dbCredentials: {
-		url: DATABASE_URL
-		// authToken: DATABASE_AUTH_TOKEN
+		url: DATABASE_URL,
+		authToken: DATABASE_AUTH_TOKEN
 	},
 	verbose: true,
 	strict: true
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,13 +1,10 @@
 import { createClient } from "@libsql/client"
 import { drizzle } from "drizzle-orm/libsql/node"
-import { DATABASE_URL, NODE_ENV } from "../config"
+import { DATABASE_AUTH_TOKEN, DATABASE_URL } from "../config"
 
 const client = createClient({
-	url:
-		NODE_ENV === "production"
-			? DATABASE_URL
-			: "file:C:/Users/oliva/Documents/Proyectos/orderburger-backend/src/db/local.db"
-	// authToken: DATABASE_AUTH_TOKEN
+	url: DATABASE_URL,
+	authToken: DATABASE_AUTH_TOKEN
 })
 
 export default drizzle(client)
