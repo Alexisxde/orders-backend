f69c1483acbf0eefacb3b2be7718130e922845f2
Alexisxde <olivarezalexis749@gmail.com>
2025-11-19 18:19:14 -0300

    refactor(auth): add refresh route to authentication endpoints

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getCurrentUser, loginUser, logoutUser, registerUser } from "../controllers/auth.controller"
+import { getCurrentUser, loginUser, logoutUser, refresh, registerUser } from "../controllers/auth.controller"
 import { schemaBodyValidator } from "../middlewares/schema-validator"
 import { verifySession } from "../middlewares/verify-session"
 import { userCreateBodySchema, userLoginBodySchema } from "../schemas/auth.schema"
@@ -9,6 +9,7 @@ const router = Router()
 router.post("/register", schemaBodyValidator(userCreateBodySchema), registerUser)
 router.post("/login", schemaBodyValidator(userLoginBodySchema), loginUser)
 router.post("/logout", logoutUser)
+router.post("/refresh", refresh)
 router.get("/user", verifySession, getCurrentUser)
 
 export default router
