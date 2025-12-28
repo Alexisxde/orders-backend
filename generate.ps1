51755386f939e09fd823b6785c4279bf65147980
Alexisxde <olivarezalexis749@gmail.com>
2025-12-28 02:04:16 -0300

    delete generate

--- generate.ps1
+++
@@ -1,9 +0,0 @@
-param([string]$Name = "file")
-
-New-Item -ItemType Directory -Force -Name $Name
-New-Item -ItemType File -Force -Path "$Name\$Name.schema.ts"
-New-Item -ItemType File -Force -Path "$Name\$Name.controller.ts"
-New-Item -ItemType File -Force -Path "$Name\$Name.service.ts"
-New-Item -ItemType File -Force -Path "$Name\$Name.route.ts"
-
-Write-Output "Carpeta y archivos creados en $Name\"
