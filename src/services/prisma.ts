43a59bfa6d2a69e8089c83845b76282ad275f573
juanRCoder <guillecarck@gmail.com>
2026-01-11 21:50:07 -0500

    fix: fixed imports

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,7 +1,7 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client";
-import { DATABASE_URL } from "@/config";
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "../../generated/prisma/client"
+import { DATABASE_URL } from "@/config"
 
-const adapter = new PrismaPg({ url: DATABASE_URL });
+const adapter = new PrismaPg({ url: DATABASE_URL })
 
-export default new PrismaClient({ adapter });
+export default new PrismaClient({ adapter })
